4d764917807d45001e2ea673647aa28da63f0b00
punkystone <email@punkystone.de>
2025-03-10 21:19:32 +0100

    update dependencies and move to flat eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+    eslintPluginPrettierRecommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "error",
+            "prettier/prettier": "error",
+        },
+    },
+);
